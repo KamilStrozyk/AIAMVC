f2e4f61b70a27c828f271a0b97c8030e1b2feaf8
Kamil Stróżyk <strozykkamil@gmail.com>
2020-05-13 21:04:42 +0200

    point 9

---
+++ AIAMVC/AIAMVC/Controllers/SongsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AIAMVC.Controllers
+{
+    public class SongsController : Controller
+    {
+        // GET: Songs
+        public ActionResult Index()
+        {
+            return Content("Hello World");
+        }
+
+        public ActionResult Square(int id)
+        {
+            return Content((id*id).ToString());
+        }
+    }
+}
